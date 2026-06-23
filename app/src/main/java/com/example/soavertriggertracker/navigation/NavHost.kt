97ee37a62edb0a8e81b4cb39776a5c71037f1202
emilijawithaj <125661251+emilijawithaj@users.noreply.github.com>
2026-06-23 20:34:22 +0100

    NavHost

---
+++ app/src/main/java/com/example/soavertriggertracker/navigation/NavHost.kt
@@ -0,0 +1,17 @@
+package com.example.soavertriggertracker.navigation
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+object HomeOverview
+@Serializable
+object MoreDetails
+@Serializable
+object LogEdit
+
+@Serializable
+object AllLogs
+@Serializable
+object AllFactors
+@Serializable
+object Triggers
